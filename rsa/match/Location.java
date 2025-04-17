2a380ffc5bd471421580fa67e8763141d7a7d9de
helena <164379029+helenamoutinho@users.noreply.github.com>
2025-04-17 14:03:57 +0100

    Update Location.java

--- rsa/match/Location.java
+++ rsa/match/Location.java
@@ -1,32 +1,37 @@
 package rsa.match;
 
-import rsa.shared.HasPoint;
+import java.io.Serializable;
+import java.util.Objects;
 
-public class Location implements HasPoint {
-    private double x;
-    private double y;
+public class Location implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    double latitude, longitude;
 
     public Location(double x, double y) {
-        this.x = x;
-        this.y = y;
+        this.longitude = x;
+        this.latitude = y;
+    }
+
+    public double x() { return longitude; }
+    public double y() { return latitude; }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Location)) return false;
+        Location other = (Location) obj;
+        return Double.compare(latitude, other.latitude) == 0 &&
+               Double.compare(longitude, other.longitude) == 0;
     }
 
     @Override
-    public double x() {
-        return 0;
+    public int hashCode() {
+        return Objects.hash(latitude, longitude);
     }
 
     @Override
-    public double y() {
-        return 0;
-    }
-
-    @Override
-    public boolean equals(Object location1) {
-        if (!(location1 instanceof Location)) return false;
-        Location location2 = (Location) this;
-        double errorMargin = 1e-6;
-        return Math.abs(this.x - location2.x()) < errorMargin &&
-                Math.abs(this.y - location2.y()) < errorMargin;
+    public String toString() {
+        return String.format("Location(%.5f, %.5f)", latitude, longitude);
     }
 }
